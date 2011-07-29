129bc07e3f07145e80bc0c68ab2a3f21e08bbad5
dougbradbury <doug@8thlight.com>
2011-07-29 16:13:57 -0500

    delete .ds

--- CppUTest/objs/tests/TestHarness_cTestCFile.d
+++
@@ -1,7 +0,0 @@
-objs/tests/TestHarness_cTestCFile.o: tests/TestHarness_cTestCFile.c \
-  include/CppUTest/MemoryLeakDetectorMallocMacros.h \
-  include/CppUTest/TestHarness_c.h
-
-include/CppUTest/MemoryLeakDetectorMallocMacros.h:
-
-include/CppUTest/TestHarness_c.h:
